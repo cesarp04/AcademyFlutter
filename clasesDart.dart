7a15c6b4b6069b9265689891ccd5c5cedd5f7249
cesar puentes <caps2596@gmail.com>
2021-05-06 11:50:27 -0500

    🚧 arreglo codigo de clases en dart

--- clasesDart.dart
+++ clasesDart.dart
@@ -1,51 +1,49 @@
-abstract class Animal {}
+abstract class Animal{}
 
-abstract class Mammal extends Animal {}
-
+abstract class Mammal extends Animal{}
 abstract class Bird extends Animal{}
 abstract class Fish extends Animal{}
 
-class Dolphin extends Mammal with SwimMixin{
-  @override
-  void walk(String text){
-    print("walking from class ${text}");
-  }
-}
-
-class Bat extends Mammal with WalkMixin, SwimMixin,FlyMixin{
-  @override
-  void walk(String text){
-    print("walking from class ${text}");
-  }
-}
-
+class Dolphin extends Mammal with SwimMixin{}
+class Bat extends Mammal with WalkMixin, FlyMixin{}
 class Cat extends Mammal with WalkMixin{}
 
-class Dove extends Mammal with FlyMixin{}
+class Dove extends Bird with FlyMixin{}
+class Duck extends Bird with WalkMixin{}
 
-class Shark extends Mammal with WalkMixin{}
+class Shark extends Fish with SwimMixin{}
+class FliyingFish extends Fish with SwimMixin{}
 
 mixin WalkMixin{
-  void walk(String text) => print("walking $text");
+   void walk( String text ) => print( "walk ${text}");
+  
+  void velocity()=> print( "is very fast");
 }
 
+  
 mixin SwimMixin{
-  void swim(String text) => print("swiming $text");
-}
+   void swim( String text ) => print( "swim ${text}");
+  
 
+}
+  
 mixin FlyMixin{
-  void fly(String text) => print("flying $text");
+   void fly( String text ) => print( "fly ${text}");
+  
 }
 
 void main(){
+
+
   Dolphin newDolphin = new Dolphin();
-  newDolphin.swim("norberto");
-
+  newDolphin.swim("Norberto");
+  
   Cat newCat = new Cat();
-  newCat.walk("onichan");
-
+  newCat.walk("inari");
+  newCat.velocity();
+  
   Bat newBat = new Bat();
-  newBat.walk("batman");
-  newBat.fly("batman");
+  newBat.fly('ricardo');
+  
 
 }
